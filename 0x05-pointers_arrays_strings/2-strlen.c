ec30d974b379ca2827ff30243904040c5bfa0003
CyrusNchege <cyruschegecc@gmail.com>
2022-12-19 14:00:23 -0800

    modified documentation for task 2

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * strlen - prints the length of a string
+ * _strlen - prints the length of a string
  * @s: pointer param to an array of characters
  * Return: always 0
  */
