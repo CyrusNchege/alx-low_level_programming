8efd31ca1ce57761bf99f5315f84b9ff50801fb0
CyrusNchege <cyruschegecc@gmail.com>
2023-01-03 14:32:13 -0800

    Removed betty warning in 4. strpbrk

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,6 +1,7 @@
 #include "main.h"
 /**
- * _strpbrk - locates the first occurrence in the string s of any of the bytes in the string accept.
+ * _strpbrk - locates the first occurrence in the string s of
+ * any of the bytes in the string accept.
  * @s: string to pick char
  * @accept: string to compare to
  * Return: string pointer
